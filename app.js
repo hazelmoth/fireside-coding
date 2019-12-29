8a54bc13bd592c7b2e21a7d32f3976dc1f1fa45a
Linus Waddell <linuswaddell@gmail.com>
2019-12-28 19:56:34 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res) {
-        res.render("views/main.ejs", {});
+        res.render("main", {});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
